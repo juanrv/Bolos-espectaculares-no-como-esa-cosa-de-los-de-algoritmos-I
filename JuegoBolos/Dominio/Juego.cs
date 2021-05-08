77536477b5ac9d0a0521a06fc8c8502c85cc1afd
Juan José Restrepo Valencia <juan.restrepo2760@uco.net.co>
2021-05-08 18:20:08 -0500

    ESTO SI ESTA BIEN HECHO HDPS

---
+++ JuegoBolos/Dominio/Juego.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoBolos.Dominio
+{
+    class Juego
+    {
+        private Jugador jugador1;
+        private Jugador jugador2;
+        private List<int[]> Lanzamientos = new List<int[]>();
+
+        public Juego(String nombreJugador1, String nombreJugador2)
+        {
+            this.jugador1 = new Jugador(nombreJugador1);
+            this.jugador2 = new Jugador(nombreJugador2);
+        }
+
+        public Jugador verJugador1()
+        {
+            return this.jugador1;
+        }
+        public Jugador verJugador2()
+        {
+            return this.jugador2;
+        }
+        public void Lanzar()
+        {
+            int turno = 1;
+            while (!(turno > 10))
+            {
+                Random generador = new Random();
+                int PinosTirados = generador.Next(0, 11);
+                jugador1.agregarPunto(PinosTirados, generador.Next(0,11 - PinosTirados));
+                PinosTirados = generador.Next(0, 11);
+                jugador2.agregarPunto(PinosTirados, generador.Next(0,11 - PinosTirados));
+                turno++;
+            }
+
+        }
+        public int[] sumatoriaPuntos(Jugador jugador)
+        {
+            int[] puntaje = new int[10];
+            int PuntajeActual = 0;
+            for(int i = 0; i < jugador.obtenerLanzamientos().Count; i+=2)
+            {
+                if(i < 16 && jugador.obtenerLanzamientos()[i] == 10 && jugador.obtenerLanzamientos()[i+2] == 10)
+                {
+                    PuntajeActual = jugador.obtenerLanzamientos()[i] + jugador.obtenerLanzamientos()[i + 2] + jugador.obtenerLanzamientos()[i + 4];   
+                }
+                else if(i < 15 && jugador.obtenerLanzamientos()[i] == 10 && jugador.obtenerLanzamientos()[i+2] != 10) 
+                {
+                    PuntajeActual = jugador.obtenerLanzamientos()[i] + jugador.obtenerLanzamientos()[i + 2] + jugador.obtenerLanzamientos()[i + 3];
+                }
+                else if(i > 11&& i < 8 &&jugador.obtenerLanzamientos()[i] == 10 && jugador.obtenerNombre()[i+2] == 10)
+                {
+                    PuntajeActual = jugador.obtenerLanzamientos()[i] + jugador.obtenerLanzamientos()[i + 2];
+                }
+                else if (i > 15 && i < 19 && jugador.obtenerLanzamientos()[i] == 10)
+                {
+                    PuntajeActual = jugador.obtenerLanzamientos()[i];
+                }
+                else
+                {
+                    PuntajeActual = jugador.obtenerLanzamientos()[i] + jugador.obtenerLanzamientos()[i + 1];
+                }
+                puntaje[i/2] = PuntajeActual; 
+
+            }
+            return puntaje;
+        }
+    }
+}
---
+++ JuegoBolos/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JuegoBolos.Dominio;
+
+namespace JuegoBolos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnJugar_Click(object sender, EventArgs e)
+        {
+            if(TextJugador1.Text.Trim().Equals("") || TextJugador2.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("Por favor ingrese el nombre de los jugadores");
+            }
+            else
+            {
+                Juego Juego = new Juego(TextJugador1.Text, TextJugador2.Text);
+                Juego.Lanzar();
+                LlenarTablas(Juego);
+            }
+        }
+
+        private void LlenarTablas(Juego Juego)
+        {
+            int[] PuntajeJugador1 = Juego.sumatoriaPuntos(Juego.verJugador1());
+            int[] PuntajeJugador2 = Juego.sumatoriaPuntos(Juego.verJugador2());
+            int[] TotalJugadores = new int[2];
+            for (int i = 0; i <= 18; i+=2)
+            {
+                TotalJugadores[0] += PuntajeJugador1[i / 2];
+                TotalJugadores[1] += PuntajeJugador2[i / 2];
+                TablaJugador1.Rows.Add((i/2)+1, 1, Juego.verJugador1().obtenerLanzamientos()[i], PuntajeJugador1[(i/2)]);
+                TablaJugador1.Rows.Add((i/2)+1, 2, Juego.verJugador1().obtenerLanzamientos()[i+1], PuntajeJugador1[(i/2)]);
+                TablaJugador2.Rows.Add((i/2) + 1, 1, Juego.verJugador2().obtenerLanzamientos()[i], PuntajeJugador2[(i/2)]);
+                TablaJugador2.Rows.Add((i / 2) + 1, 2, Juego.verJugador2().obtenerLanzamientos()[i + 1], PuntajeJugador2[(i / 2)]);
+                
+            }
+            TablaJugador1.Rows.Add("-", "-", "-", TotalJugadores[0]);
+            TablaJugador2.Rows.Add("-", "-", "-", TotalJugadores[1]);
+
+            if (TotalJugadores[0] > TotalJugadores[1])
+            {
+                MessageBox.Show("El jugador: " + Juego.verJugador1().obtenerNombre() + Environment.NewLine + "¡HA GANADO!");
+            }
+            else
+            {
+                MessageBox.Show("El jugador: " + Juego.verJugador2().obtenerNombre() + Environment.NewLine + "¡HA GANADO!");
+
+            }
+
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TablaJugador1.Rows.Clear();
+            TablaJugador2.Rows.Clear();
+            TextJugador1.Clear();
+            TextJugador2.Clear();
+        }
+    }
+}
